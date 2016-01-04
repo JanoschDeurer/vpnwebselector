5430e33b6a06230c9fcd04b51f8a822f80d82c5d
Janosch Deurer <janosch.deurer@geonautik.de>
2016-01-04 21:39:49 +0100

    Fixes pylint errors

--- vpnwebselector.py
+++ vpnwebselector.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""Tool to easely setup a vpn connection
+
+This script searches for vpn configuration files in the configs folder. It
+starts a webserver and provides a web gui to select one of these configs
+connect to the corresponding server or reset the connection.
+
+Author: Janosch Deurer
+E-Mail: janosch.deurer@geonautik.de
+
+When executing the script, you have to make shure that the executing user hase
+the previleges to create openvpn connections, this can for example be reached
+by using sudo. The script can be executed by:
+    ./vpnwebselector.py
+
+
+Dependencies:
+    The script is witten in python 3 it was tested with python 3.4.3.
+    It furthermore uses openvpn it was tested wit openvpn version 2.3.7
+"""
+
+
 import http.server
 import os
 import re
@@ -7,11 +28,15 @@ import subprocess
 
 
 class VpnWebSelectorHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    """
+    Handles http requests for the vpnwebselector
+    """
 
-    """Docstring for VpnWebSelectorHTTPRequestHandler. """
+    # Disable [too-many-public-methods] error as this is caused by inheritance.
+    # pylint: disable=R0904
 
     def do_GET(self):  # noqa
-        if re.match("/getConfigs(\?.*|$)", self.path):
+        if re.match(r"/getConfigs(\?.*|$)", self.path):
             self.response()
             self.get_querry_results()
             return
@@ -35,9 +60,9 @@ class VpnWebSelectorHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.server.clear_selected_config()
             return
 
-        if re.match("/setConfig(\?.*|$)", self.path):
+        if re.match(r"/setConfig(\?.*|$)", self.path):
             self.response()
-            querry = re.findall("/setConfig\?q=(.*)", self.path)[0]
+            querry = re.findall(r"/setConfig\?q=(.*)", self.path)[0]
             self.server.selected_config = querry
             return
 
@@ -52,13 +77,24 @@ class VpnWebSelectorHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         super().end_headers()
 
     def response(self):
+        """
+        Makes a standard 200 Response with
+        content-type text/html
+        :returns: None
+        """
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
 
     def get_querry_results(self):
-        querry = re.findall("/getConfigs\?q=(.*)", self.path)[0]
-        files = self.server.get_configs()
+        """
+        Find all vpn confings in the configs folder that match the description
+        given in the get request. The matching is done case insensitive and
+        with no typos allowed. The result is returned directly to the client.
+        :returns: None
+        """
+        querry = re.findall(r"/getConfigs\?q=(.*)", self.path)[0]
+        files = get_configs()
         results = [f for f in files if querry.lower() in f.lower()]
         results.sort(key=len)
         results = json.dumps(results)
@@ -67,34 +103,44 @@ class VpnWebSelectorHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
 
 
 class VpnWebSelectorHTTPServer(http.server.HTTPServer):
-
-    """Docstring for VpnWebSelectorHTTPServer. """
-
+    """
+    HTTPServer for the vpnwebselector
+    """
     def __init__(self, server_address, handler_class):
-        """TODO: to be defined1. """
         self._selected_config = ""
         self._last_selected_config = ""
         self._openvpn_process = None
         self._output_file = None
         super().__init__(server_address, handler_class)
 
-    def get_configs(self):
-        path = os.path.join(os.path.pardir, "configs")
-        return [conf for conf in os.listdir(path) if conf.endswith(".ovpn")]
-
     @property
     def selected_config(self):
+        """
+        Getter for _selected_config
+        :returns: _selected_config
+        """
         return self._selected_config
 
     @selected_config.setter
     def selected_config(self, val):
-        if val in self.get_configs():
+        """
+        Setter for _selected_config. This first checks if val is a valid value
+        for _selected_config, if so it sets the config and changes the
+        connection to the new _selected_config.
+        :returns: None
+        """
+        if val in get_configs():
             self._last_selected_config = self._selected_config
             self._selected_config = val
             if self._last_selected_config != self._selected_config:
                 self.connect_to_vpn_server()
 
     def connect_to_vpn_server(self):
+        """
+        Starts an openvpn process with _selected_config as config and saves the
+        process in _openvpn_process.
+        :returns: None
+        """
         if self._selected_config is None:
             return
         self.terminate_vpn_connection()
@@ -106,6 +152,10 @@ class VpnWebSelectorHTTPServer(http.server.HTTPServer):
                                                  stderr=subprocess.STDOUT)
 
     def terminate_vpn_connection(self):
+        """
+        Exits the _openvpn_process if one exists.
+        :returns: None
+        """
         self._output_file = open('output.txt', 'w')
         if self._openvpn_process is None:
             return
@@ -116,8 +166,18 @@ class VpnWebSelectorHTTPServer(http.server.HTTPServer):
     def clear_selected_config(self):
         """
         Reset the _selected_config to None
+        :returns: None
         """
         self._selected_config = None
+
+
+def get_configs():
+    """
+    Get a list of all configs in the configs directory ending with .ovpn
+    :returns: List with configs
+    """
+    path = os.path.join(os.path.pardir, "configs")
+    return [conf for conf in os.listdir(path) if conf.endswith(".ovpn")]
 
 
 def main():
